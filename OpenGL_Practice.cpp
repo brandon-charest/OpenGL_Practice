d65181009b850b14a793171cf9f345d750d824d0
brandon <brandon_charest@yahoo.com>
2018-01-04 14:40:20 -0500

    display window

--- OpenGL_Practice.cpp
+++ OpenGL_Practice.cpp
@@ -1,16 +1,77 @@
 // OpenGL_Practice.cpp : Defines the entry point for the console application.
 //
-#include  <glad.h>
+//GLAD manages function pointers for OpenGL
+#include <glad.h>
 #include <GLFW\glfw3.h>
+
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+void processInput(GLFWwindow *window);
+
+//window settings
+const unsigned int SCREEN_WIDTH = 800;
+const unsigned int SCREEN_HEIGHT = 600;
 
 int main()
 {
 	
+	// glfw: initialize and configure
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+	// glfw: window creation
+	GLFWwindow *window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Learn OpenGL", NULL, NULL);
+	if(window == NULL)
+	{
+		std::cout << "Failed to create GLFW window." << std::endl;
+		glfwTerminate();
+
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);	
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	
+	// glad: load all OpenGL function pointers
+	if (!gladLoadGLLoader(GLADloadproc(glfwGetProcAddress)))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+
+		return -1;
+	}
+
+	// render loop
+	while (!glfwWindowShouldClose(window))
+	{
+		// input
+		processInput(window);
+
+		// render
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	// glfw: terminate, clearing all previously allocated GLFW resources.	
+	glfwTerminate();
 	return 0;
 }
 
+// glfw: handle user resizing of window
+void framebuffer_size_callback(GLFWwindow* window, const int width, const int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+// process all input
+void processInput(GLFWwindow *window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
---
+++ OpenGL_Practice.h
@@ -0,0 +1,12 @@
+#pragma once
+
+class Window
+{
+public:
+	//window settings
+	const unsigned int SCREEN_WIDTH = 800;
+	const unsigned int SCREEN_HEIGHT = 600;
+private:
+	
+
+};
